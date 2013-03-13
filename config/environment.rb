5a6a405bda1f0c75a0db5bf9f218e999db69f3c1
Yu Jiang Tham <ytham@apple.com>
2013-03-13 10:16:00 -0700

    smtp settings

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 MailTester::Application.initialize!
 
-config.action_mailer.default_url_options = { :host => 'localhost' }
+#config.action_mailer.default_url_options = { :host => 'localhost' }
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,6 +15,8 @@ MailTester::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host: '127.0.0.1', port: 3000 }
 
   # Print deprecation notices to the Rails logger
   config.active_support.deprecation = :log
