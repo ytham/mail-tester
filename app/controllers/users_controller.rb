cf9545d8a4e350cddab5e699d59e5e11acf727a8
Yu Jiang Tham <ytham@apple.com>
2013-03-12 23:33:37 -0700

    end tag removed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
           format.json { render json: @user.errors, status: :unprocessable_entity }
         end
       end
-    end
+    #end
   end
 
   # PUT /users/1
